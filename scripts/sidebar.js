cfbc9a239342f63db2ab272f9091b7fc191f89c7
mixanBit <65313661+mixanBit@users.noreply.github.com>
2022-07-15 18:08:09 +0300

    Sidebar

---
+++ scripts/sidebar.js
@@ -0,0 +1,23 @@
+let sidebar = document.querySelector('.sidebar')
+let sidebarBtn = document.querySelector('.sidebar__btn')
+let sidebarFon = document.querySelector('.sidebar-fon')
+
+sidebarBtn.addEventListener('click', () => {
+  if(sidebar.classList.contains("sidebar_active")){
+    sidebar.classList.remove('sidebar_active')
+    sidebarFon.classList.remove('sidebar-fon_active')
+    console.log('hello');
+  } else{
+    sidebar.classList.add('sidebar_active')
+    sidebarFon.classList.add('sidebar-fon_active')
+  }
+})
+
+
+
+sidebarFon.addEventListener('click', (el) => {
+  if(el.target.classList.contains("sidebar-fon_active") || el.target.classList.contains("sidebar__btn")){
+    sidebar.classList.remove('sidebar_active')
+    sidebarFon.classList.remove('sidebar-fon_active')
+  }
+})
