e76ebf1538f829eaf169942f41b5c7d42a497f99
mixanBit <65313661+mixanBit@users.noreply.github.com>
2022-07-17 15:24:25 +0300

    Decisions block

--- scripts/slick-settings.js
+++ scripts/slick-settings.js
@@ -3,5 +3,19 @@ $('.multiple-items').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   nextArrow: $('.decisions__next-slide'),
-  prevArrow: $('.decisions__prev-slide')
+  prevArrow: $('.decisions__prev-slide'),
+  responsive: [
+    {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2
+        }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1
+      }
+  }
+  ]
 });
